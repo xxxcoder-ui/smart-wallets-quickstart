41f1704e415948ccb9eeda044bd601bc1131920a
Ava Robinson <avamrobinson@gmail.com>
2024-07-10 17:33:54 -0400

    removing routes complexity and hard coding api key

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ import { AlchemyAccountsProviderProps } from "@account-kit/react";
 import { QueryClient } from "@tanstack/react-query";
 
 export const config = createConfig({
-  rpcUrl: "/api/rpc", // NOTE: you must replace the API key in the .env file to your apps API key
+  apiKey: "YOUR_API_KEY",
   chain: sepolia,
   ssr: true, // defers hydration of the account state to the client after the initial mount solving any inconsistencies between server and client state (read more here: https://accountkit.alchemy.com/react/ssr)
   storage: cookieStorage, // persist the account state using cookies (read more here: https://accountkit.alchemy.com/react/ssr#persisting-the-account-state)
@@ -14,7 +14,8 @@ export const uiConfig: AlchemyAccountsProviderProps["uiConfig"] = {
   auth: {
     sections: [
       [{ type: "email" }],
-      [{ type: "passkey" }, { type: "injected" }],
+      [{ type: "passkey" }],
+      // [{ type: "injected" }],
     ],
     addPasskeyOnSignup: true,
   },
