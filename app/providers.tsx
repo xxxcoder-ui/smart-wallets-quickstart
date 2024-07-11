d14b02ef028b6b288c2a4e49a34493c00891afd0
Ava Robinson <avamrobinson@gmail.com>
2024-07-11 14:58:05 -0400

    updating to v4 alpha version with nested ui components

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { config, queryClient, uiConfig } from "@/config";
+import { config, queryClient } from "@/config";
 import { AlchemyClientState } from "@account-kit/core";
 import { AlchemyAccountProvider } from "@account-kit/react";
 import { QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +14,6 @@ export const Providers = (
         <AlchemyAccountProvider
           config={config}
           queryClient={queryClient}
-          uiConfig={uiConfig}
           initialState={props.initialState}
         >
           {props.children}
