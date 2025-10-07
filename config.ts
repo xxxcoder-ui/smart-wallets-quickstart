d2da8c00dbb920b3925da02ae412e7c08f4a6012
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-07 10:26:44 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -6,6 +6,31 @@ import {
 import { alchemy, arbitrumSepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
+import { AlchemyAccountsUIConfig, createConfig } from "@account-kit/react";
+import { sepolia, alchemy } from "@account-kit/infra";
+const uiConfig: AlchemyAccountsUIConfig = {
+  auth: {
+    sections: [
+      [
+        {
+          type: "email",
+          emailMode: "magicLink",
+          // Optional customizations:
+          buttonLabel: "Continue with Email",
+          placeholder: "Enter your email address",
+        },
+      ],
+    ],
+  },
+};
+export const config = createConfig(
+  {
+    transport: alchemy({ apiKey: "e60ay6ivqy39fbnq7nv21hf0iinwky2e" }),
+    chain: sepolia,
+  },
+  uiConfig,
+);
+
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 if (!API_KEY) {
   throw new Error("NEXT_PUBLIC_ALCHEMY_API_KEY is not set");
@@ -27,19 +52,19 @@ const uiConfig: AlchemyAccountsUIConfig = {
         { type: "social", authProviderId: "facebook", mode: "popup" },
       ],
     ],
-    addPasskeyOnSignup: false,
+    addPasskeyOnSignup: true,
   },
 };
 
 export const config = createConfig(
   {
-    transport: alchemy({ apiKey: API_KEY }),
+    transport: alchemy({ apiKey: e60ay6ivqy39fbnq7nv21hf0iinwky2e }),
     // Note: This quickstart is configured for Arbitrum Sepolia.
-    chain: arbitrumSepolia,
+    chain: Sepolia,
     ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
     storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
-    policyId: SPONSORSHIP_POLICY_ID,
+    policyId: 560c46c1-0a93-48b2-8c9a-dad0b337dddb,
   },
   uiConfig
 );
