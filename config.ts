7f13e46ab56da11b64dfc78e1f9487481380728e
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-08 19:16:07 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import {
   cookieStorage,
   createConfig,
 } from "@account-kit/react";
-import { alchemy, polygonamoy } from "@account-kit/infra";
+import { alchemy, sepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
@@ -35,7 +35,7 @@ export const config = createConfig(
   {
     transport: alchemy({ apiKey: API_KEY }),
     // Note: This quickstart is configured for Arbitrum Sepolia.
-    chain: polygonamoy,
+    chain: sepolia,
     ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
     storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
@@ -43,5 +43,15 @@ export const config = createConfig(
   },
   uiConfig
 );
+
+import { type Address, toHex } from "viem";
+
+export const config = {
+  policyId: process.env.ALCHEMY_POLICY_ID!,
+  gasToken: "0xD3b483Ed22B20858313dC420b6858Ab2bca4a562" as Address, // USDC on Sepolia
+  approveBelow: toHex(1000000n), // 1 USDC
+  approveAmount: toHex(10000000n), // 10 USDC
+};
+
 
 export const queryClient = new QueryClient();
