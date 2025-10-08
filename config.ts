850bc75894892baa04aa8e6469e224ef1280f362
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-08 18:52:06 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,34 +1,10 @@
 import {
   AlchemyAccountsUIConfig,
   cookieStorage,
+  createConfig,
 } from "@account-kit/react";
-import { createConfig } from "@account-kit/core";
-import { sepolia, mainnet, alchemy } from "@account-kit/infra";
+import { alchemy, polygonamoy } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
-import { setChain } from "@account-kit/core";
-
-export const config = createConfig({
-  // use this transport for all chains
-  transport: alchemy({ apiKey: "ALCHEMY_API_KEY" }),
-  // this is the default chain
-  chain: sepolia,
-  storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
-  enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
-  chains: [
-    {
-      chain: mainnet,
-      // optional: sponsor gas for this chain
-      policyId: "MAINNET_GAS_MANAGER_POLICY_ID",
-    },
-    {
-      chain: sepolia,
-      // optional: override the default transport for this chain
-      transport: alchemy({ apiKey: "OTHER_API_KEY" }),
-      // optional: sponsor gas for this chain
-      policyId: "SEPOLIA_GAS_MANAGER_POLICY_ID",
-    },
-  ],
-});
 
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
 if (!API_KEY) {
@@ -55,8 +31,17 @@ const uiConfig: AlchemyAccountsUIConfig = {
   },
 };
 
-
+export const config = createConfig(
+  {
+    transport: alchemy({ apiKey: API_KEY }),
+    // Note: This quickstart is configured for Arbitrum Sepolia.
+    chain: polygonamoy,
+    ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
+    storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
+    enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
+    policyId: SPONSORSHIP_POLICY_ID,
+  },
+  uiConfig
+);
 
 export const queryClient = new QueryClient();
-
-await setChain(config, mainnet);
