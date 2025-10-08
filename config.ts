71e65fc91fa0d85695715e020bb73bdcea01e32d
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-08 19:55:17 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -27,7 +27,7 @@ const uiConfig: AlchemyAccountsUIConfig = {
         { type: "social", authProviderId: "facebook", mode: "popup" },
       ],
     ],
-    addPasskeyOnSignup: true,
+    addPasskeyOnSignup: false,
   },
 };
 
