d2f42ed69025dcd5d3e1ebdd7a06f7e9c1fb3518
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-08 20:14:26 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import {
   cookieStorage,
   createConfig,
 } from "@account-kit/react";
-import { alchemy, sepolia } from "@account-kit/infra";
+import { alchemy, arbitrumsepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
@@ -35,7 +35,7 @@ export const config = createConfig(
   {
     transport: alchemy({ apiKey: API_KEY }),
     // Note: This quickstart is configured for Arbitrum Sepolia.
-    chain: sepolia,
+    chain: arbitrumsepolia,
     ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
     storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
     enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
