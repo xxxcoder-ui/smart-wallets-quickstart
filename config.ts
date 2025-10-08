bf0c2c98f3af42c9b4abec4561f714aa1b4b5038
xxxcoder-ui <62079682+xxxcoder-ui@users.noreply.github.com>
2025-10-08 16:16:00 +0100

    Update config.ts

--- config.ts
+++ config.ts
@@ -3,7 +3,7 @@ import {
   cookieStorage,
   createConfig,
 } from "@account-kit/react";
-import { alchemy, Sepolia } from "@account-kit/infra";
+import { alchemy, arbitrumSepolia } from "@account-kit/infra";
 import { QueryClient } from "@tanstack/react-query";
 
 const API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
@@ -31,6 +31,17 @@ const uiConfig: AlchemyAccountsUIConfig = {
   },
 };
 
-
+export const config = createConfig(
+  {
+    transport: alchemy({ apiKey: API_KEY }),
+    // Note: This quickstart is configured for Arbitrum Sepolia.
+    chain: arbitrumSepolia,
+    ssr: true, // more about ssr: https://www.alchemy.com/docs/wallets/react/ssr
+    storage: cookieStorage, // more about persisting state with cookies: https://www.alchemy.com/docs/wallets/react/ssr#persisting-the-account-state
+    enablePopupOauth: true, // must be set to "true" if you plan on using popup rather than redirect in the social login flow
+    policyId: SPONSORSHIP_POLICY_ID,
+  },
+  uiConfig
+);
 
 export const queryClient = new QueryClient();
